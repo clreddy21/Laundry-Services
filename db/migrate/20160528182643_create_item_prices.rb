4a6132a9d25ebda00075f5512339d5d3f2260232
clreddy <c.l.r.421@gmail.com>
2016-06-08 22:07:17 +0530

    minor refactoring

--- db/migrate/20160528182643_create_item_prices.rb
+++ db/migrate/20160528182643_create_item_prices.rb
@@ -1,9 +1,10 @@
 class CreateItemPrices < ActiveRecord::Migration
   def change
     create_table :item_prices do |t|
-      t.references :service_provider, index: true, foreign_key: true
+
       t.references :item, index: true, foreign_key: true
       t.references :service_type, index: true, foreign_key: true
+      t.integer :service_provider_id
       t.integer :price
 
       t.timestamps null: false
