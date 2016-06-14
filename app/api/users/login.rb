7281c119a19c9fb2b0ee080dc0985b721b8c2098
clreddy <c.l.r.421@gmail.com>
2016-06-14 22:40:18 +0530

    minor refactoring

--- app/api/users/login.rb
+++ app/api/users/login.rb
@@ -18,7 +18,8 @@ module Users
 						rsa_public = rsa_private.public_key
 						token = JWT.encode payload, rsa_private, 'RS256'
 						user.update(jwt: token)
-						{:success => true, :email => params[:email], :token => user.jwt, :name => user.first_name + user.last_name, :mobile_number => user.mobile}
+						{:success => true, :email => params[:email], :token => user.jwt, :name => user.first_name + user.last_name,
+						 :mobile_number => user.mobile, :user_id => user.id}
 					else
 						{:success => false, :message => "Incorrect Password"}
 					end
