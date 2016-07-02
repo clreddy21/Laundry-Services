610d20feac963ddbed1ce34cb623d6a882ff21ca
clreddy <c.l.r.421@gmail.com>
2016-07-02 11:34:59 +0530

    minor refactoring

--- app/api/service_provider_details/item_prices.rb
+++ app/api/service_provider_details/item_prices.rb
@@ -19,8 +19,40 @@ module ServiceProviderDetails
                                                 service_type_id: item_price[:service_type_id], price: item_price[:price])
           end
 
-          message = 'Item price successfully update for service provider.'
+          message = 'Item price successfully added for service provider.'
           
+          options = {data: {'messageType' => 'list','message' => message,'title' => 'Laundry Services'}}
+
+          service_provider.send_mobile_notification(options)
+          {:message => message, :success => true}
+        end
+      end
+    end
+
+
+    resource :update_item_prices do
+      desc 'Updating item prices of Service provider'
+      params do
+        requires :sp_id, type:Integer
+        requires :item_prices, type:Array
+      end
+
+      post do
+        service_provider = ServiceProvider.includes(:item_prices).find(params[:sp_id])
+        if service_provider.nil?
+          {:message => 'please send a valid service provider id', :success => false}
+        else
+          item_prices = params[:item_prices]
+          item_prices.each do |item_price|
+            ip = ItemPrice.find(item_price[:item_price_id])
+
+            if service_provider.item_prices.include? ip
+              ip.update(price: item_price[:price])
+            end
+          end
+
+          message = 'Item price successfully updated for service provider.'
+
           options = {data: {'messageType' => 'list','message' => message,'title' => 'Laundry Services'}}
 
           service_provider.send_mobile_notification(options)
