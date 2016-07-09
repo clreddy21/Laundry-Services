737fa1b1e1349a609cd3bb6df2c27a130edff254
clreddy <c.l.r.421@gmail.com>
2016-07-10 00:26:13 +0530

    added status to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709142014) do
+ActiveRecord::Schema.define(version: 20160709185212) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "address"
@@ -195,6 +195,7 @@ ActiveRecord::Schema.define(version: 20160709142014) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "status"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
