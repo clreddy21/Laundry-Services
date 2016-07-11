1491e70ad28583c2db702171fab0d78af6c3b920
clreddy21 <c.l.r.421@gmail.com>
2016-07-11 09:35:55 +0530

    devise login options modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
+
   has_one :address, as: :addressable
   has_one :wallet
   has_many :refunds
@@ -71,6 +73,22 @@ class User < ActiveRecord::Base
     false
   end
 
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.mobile || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["mobile = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:mobile) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 
   private
 
