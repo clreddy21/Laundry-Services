bb30cc1744463bd3b0415a0e31ab5405f5e7f879
clreddy21 <c.l.r.421@gmail.com>
2016-07-15 10:22:37 +0530

    added a service to get comments of order item by any user.

--- app/api/users/orders.rb
+++ app/api/users/orders.rb
@@ -4,8 +4,8 @@ module Users
       desc 'Adding a comment to order item'
 			params do
 			  requires :comment, type:String
-			  requires :comment_by_id, type:String
-			  requires :order_item_id, type:String
+			  requires :comment_by_id, type:Integer
+			  requires :order_item_id, type:Integer
 			end
 			## This takes care of creating user's jwt
 			post do
@@ -26,5 +26,34 @@ module Users
 			  end
 			end
 		end
+
+    resource :get_comments_of_order_item do
+      desc 'Getting comments of an order item'
+			params do
+			  requires :order_item_id, type:Integer
+			end
+			## This takes care of creating user's jwt
+			get do
+				puts params.inspect
+
+		    if OrderItem.exists?(id: params[:order_item_id])
+		    	order_item = OrderItem.includes(:order_comments).find_by(id: params[:order_item_id])
+
+		    	order_comments = []
+		    	order_item.order_comments.each do |comment|
+		    		order_comments << {id: comment.id, body: comment.body, comment_by_id: comment.comment_by_id,
+		    		 comment_by_type: comment.comment_by_type, created_at: comment.created_at}
+		    	end
+		    	
+		    	{success: true, order_comments: order_comments}
+
+				else
+					{:success => false, :message => 'Invalid order item id.'}
+			  end
+			end
+		end
+
+
+
 	end
 end
