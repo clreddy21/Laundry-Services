e2a6c23bd3286ccfaab4cef439ab6e2d723c5674
clreddy <c.l.r.421@gmail.com>
2016-07-17 08:05:33 +0530

    added lot of things i dont remember the list

--- app/assets/javascripts/common/common.js
+++ app/assets/javascripts/common/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.fa-clock-o').click(function(){
         $(this).siblings('[type="text"]').focus().click();
-    })
+    });
 
     $('.best_in_place').best_in_place()
 });
