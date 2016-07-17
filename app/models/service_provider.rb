65c150e319786ba6b384ad442b4645f5ff2853ba
clreddy <c.l.r.421@gmail.com>
2016-07-17 08:55:39 +0530

    minor refactoring

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -3,12 +3,17 @@ class ServiceProvider < User
   has_many :orders
   has_many :orders_comments
   has_many :reviews, as: :reviewable
+  # after_save :build_item_prices
 
   def service_provider_stats
     orders_count = self.orders.size
     total_cost = self.orders.pluck(:total_cost).sum
     {orders_count: orders_count, total_cost: total_cost}
   end
+
+  # def build_item_prices
+  #   raise self.id
+  # end
 
 
 end
