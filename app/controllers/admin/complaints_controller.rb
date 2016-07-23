223a8129eb25ad8822286b5bf399db2564ac8a4d
loki <c.l.r.421@gmail.com>
2016-07-23 23:22:46 +0530

    added links to complaints show page in complaints index page added basic order details in complaints show page added reference id in order show page

--- app/controllers/admin/complaints_controller.rb
+++ app/controllers/admin/complaints_controller.rb
@@ -5,4 +5,8 @@ class Admin::ComplaintsController < ApplicationController
     @complaints = Complaint.all.includes(:order, :messages)
   end
 
+  def show
+    @complaint = Complaint.find_by(id: params[:id])
+    redirect_to list_of_admin_complaints_path if @complaint.nil?
+  end
 end
