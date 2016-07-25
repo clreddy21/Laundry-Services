924dcb24ddeb304ad632a3abd6b22ce8ab8f2d60
loki <c.l.r.421@gmail.com>
2016-07-25 23:55:16 +0530

    added last updated date for service provider item_ prices service

--- app/api/customers/service_providers.rb
+++ app/api/customers/service_providers.rb
@@ -58,7 +58,7 @@ module Customers
             item_item_prices = item_prices.where(:item_id => item.id)
             item_item_prices.each do |item_item_price|
               prices << {service_type_id: item_item_price.service_type_id, service_type_name: item_item_price.service_type.name,
-               price: item_item_price.price}
+               price: item_item_price.price, last_updated_date: item_item_price.updated_at}
             end
             item_prices_hash << {item_id: item.id, item_name: item.name, prices: prices}
           end
