62e238d44fcdbfccecca689a7011bb125958c556
loki <c.l.r.421@gmail.com>
2016-07-25 23:53:58 +0530

    added list of complaints of user and complaint details services for complaints

--- app/api/complaints/complaints.rb
+++ app/api/complaints/complaints.rb
@@ -2,6 +2,42 @@ module Complaints
   class Complaints < Grape::API
 
 
+    resource :list_of_complaints_of_a_user do
+      desc 'list of complaints of a user'
+      params do
+        requires :user_id, type: Integer
+      end
+      get do
+        user = User.find_by_id(params[:user_id])
+        if user.nil?
+          {success: false, message: 'Invalid User ID'}
+        else
+          order_ids = user.orders.pluck(:id)
+          complaints = Complaint.where(order_id: order_ids)
+          complaints.select(:id, :reference_id, :status, :order_id, :title)
+        end
+      end
+    end
+
+
+    resource :complaint_details do
+      desc 'complaint details'
+      params do
+        requires :complaint_id, type: Integer
+      end
+      get do
+        complaint = Complaint.includes(:messages).find_by_id(params[:complaint_id])
+        if complaint.nil?
+          {success: false, message: 'Invalid Complaint ID'}
+        else
+          complaint_messages = complaint.messages.select(:body, :user_id)
+          {id: complaint.id, reference_id: complaint.reference_id, status: complaint.status, order_id: complaint.order_id,
+           title: complaint.title, comments: complaint_messages}
+
+        end
+      end
+    end
+
     resource :new_complaint do
       desc 'creating new complaint'
       params do
