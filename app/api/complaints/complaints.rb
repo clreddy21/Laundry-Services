7cec5fb84aaee3fb7b7addb7c4fc3a273209bb94
loki <c.l.r.421@gmail.com>
2016-07-27 07:49:44 +0530

    did some changes in adding comment to complaint and creating it for a order

--- app/api/complaints/complaints.rb
+++ app/api/complaints/complaints.rb
@@ -23,13 +23,19 @@ module Complaints
     resource :complaint_details do
       desc 'complaint details'
       params do
-        requires :complaint_id, type: Integer
+        requires :order_id, type: Integer
       end
       get do
-        complaint = Complaint.includes(:messages).find_by_id(params[:complaint_id])
-        if complaint.nil?
-          {success: false, message: 'Invalid Complaint ID'}
+        order = Order.find_by_id(params[:order_id])
+
+        if order.nil?
+          {success: false, message: 'Invalid Order ID'}
         else
+          if order.complaints.present?
+            complaint = order.complaints.includes(:messages).last
+          else
+            complaint = order.complaints.create(status: 'pending')
+          end
           complaint_messages = complaint.messages.select(:body, :user_id)
           {id: complaint.id, reference_id: complaint.reference_id, status: complaint.status, order_id: complaint.order_id,
            title: complaint.title, comments: complaint_messages}
@@ -38,49 +44,29 @@ module Complaints
       end
     end
 
-    resource :new_complaint do
-      desc 'creating new complaint'
-      params do
-        requires :order_id, type: Integer
-        requires :title, type: String
-        requires :message, type: String
-        requires :user_id, type: Integer
-      end
-      post do
-        user = User.find_by_id(params[:user_id])
-        order = Order.find_by_id(params[:order_id])
-        if user.nil?
-          {success: false, message: 'Invalid User ID'}
-        elsif order.nil?
-          {success: false, message: 'Invalid Order ID'}
-        else
-          complaint = order.complaints.create(title: params[:title], status: 'pending')
-          complaint.save!
-          complaint.create_reference_id
-          Message.create(body: params[:message], user_id: user.id, messageable: complaint)
-          {success: true, message: 'Complaint created and a Message added successfully to complaint.',
-           complaint_reference_id: complaint.reference_id, complaint_id: complaint.id, complaint_status: complaint.status}
-        end
-      end
-    end
-
-
     resource :add_message_to_complaint do
       desc 'adding messages to a complaint'
       params do
         requires :message, type: String
         requires :user_id, type: Integer
-        requires :complaint_id, type: Integer
+        requires :order_id, type: Integer
       end
       post do
         user = User.find_by_id(params[:user_id])
-        complaint = Complaint.find_by_id(params[:complaint_id])
+        order = Order.find_by_id(params[:order_id])
+
 
         if user.nil?
           {success: false, message: 'Invalid User ID'}
-        elsif complaint.nil?
-          {success: false, message: 'Invalid Complaint Id'}
+        elsif order.nil?
+          {success: false, message: 'Invalid Order Id'}
         else
+          if order.complaints.present?
+            complaint = order.complaints.includes(:messages).last
+          else
+            complaint = order.complaints.create(status: 'pending')
+          end
+
           Message.create(body: params[:message], user_id: user.id, messageable: complaint)
           {success: true, message: 'Message added successfully to complaint.',
            complaint_reference_id: complaint.reference_id, complaint_status: complaint.status}
