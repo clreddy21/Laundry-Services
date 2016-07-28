97e6e83b241f758c76296f4d7aacb6bbb83e5da5
loki <c.l.r.421@gmail.com>
2016-07-28 23:45:00 +0530

    complaints js

---
+++ app/assets/javascripts/complaints.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+   $('.complaint-change-status').click(function(){
+       $('.resolve_issue').removeClass('hide');
+   })
+
+   $('.complaint-approved').click(function(){
+       $('.add_money_to_customer').removeClass('hide');
+   })
+
+
+});
