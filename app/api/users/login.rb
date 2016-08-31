07a9474b44f6c7415e203d4f5d4b1314a2ccb3e8
loki <c.l.r.421@gmail.com>
2016-08-31 10:24:24 +0530

    minor refactoring

--- app/api/users/login.rb
+++ app/api/users/login.rb
@@ -18,7 +18,7 @@ module Users
 						rsa_public = rsa_private.public_key
 						token = JWT.encode payload, rsa_private, 'RS256'
 						user.update(jwt: token)
-						address = user.addresses.present ? user.addresses.first.address : ''
+						address = user.addresses.present? ? user.addresses.first.address : ''
 
 						if user.is_customer?
 							wallet_amount = user.wallet.amount.to_i
